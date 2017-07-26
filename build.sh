742184dafa582893123e63bfa30a77098b853fe4
Jordan Majd <jordan@madtitanstudios.com>
2017-07-25 22:50:07 -0600

    Fix build flag, capitilize i

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo '[objects]' > temp
 echo Main.obj >> temp
 
 # Avengers assemble!
-wla-65816 -o $MAIN.obj src/$MAIN.asm
+wla-65816 -I src -o $MAIN.obj src/$MAIN.asm
 wlalink -v temp bin/$MAIN.smc
 
 # Clean up build files
