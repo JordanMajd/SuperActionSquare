690cc02d257e17a68cb81127e7458c63dbf2653a
Jordan Majd <jordan@madtitanstudios.com>
2017-07-25 21:52:51 -0600

    Hello SNES.

---
+++ src/Main.asm
@@ -0,0 +1,28 @@
+;== Main.asm ==
+; Includes
+.INCLUDE "Header.inc"
+.INCLUDE "SnesINIT.asm"
+
+;== Start Start ==
+
+.BANK 0 SLOT 0
+.ORG 0
+.SECTION "MainCode"
+
+Start:
+	InitSNES
+
+	stz $2121		; Screen color, write in binary or hex.
+	lda #$1F		; = 00011111
+	sta $2122
+	stz $2122		; second byte has no data so write 0.
+
+	lda #$0F		; = 00001111
+	sta $2100		; Turn screen on with full brightness.
+
+Forever:
+	jmp Forever
+
+.ENDS
+
+;== End Start ==
